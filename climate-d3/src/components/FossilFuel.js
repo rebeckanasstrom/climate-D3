1e713bd0fc9b5ed2277dba62a12862b7cb313817
rebeckanasstrom <56342628+rebeckanasstrom@users.noreply.github.com>
2019-12-05 14:22:22 +0100

    Added Fossil Fuel Line Chart
    Added Line Chart for Fossil Fuel page by creating FossilLine (line chart) and FossilData (data from API) components, rendering them in FossilData so that the chart shows when you click the Fossil Fuel Page in Nav/Cards.
    
    Co-Authored-By: malinaastrom <malinaastrom@users.noreply.github.com>
    Co-Authored-By: annaahlobucina <annaahlobucina@users.noreply.github.com>

--- climate-d3/src/components/FossilFuel.js
+++ climate-d3/src/components/FossilFuel.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 import '../App.css';
+import FossilData from './FossilData';
 
 export default class FossilFuel extends Component {
   render(){ 
     return (
     <div>
-      <h1>Fossil Fuel</h1>
-      <p>Hej, det här är en annan text.</p>
+      <FossilData />
     </div>
     )
 }
