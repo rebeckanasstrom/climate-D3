be441911108de8b0773ba754bad9f45bbf5e1ae7
malinaastrom <56342796+malinaastrom@users.noreply.github.com>
2020-01-03 11:35:51 +0100

    Changed the menu
    Changed the menu with semantic ui so it worked better (less code) + CSS. Made the menu sticky.
    
    Co-Authored-By: josefineingemarsson <josefineingemarsson@users.noreply.github.com>
    Co-Authored-By: rebeckanasstrom <rebeckanasstrom@users.noreply.github.com>

--- climate-d3/src/components/Nav.js
+++ climate-d3/src/components/Nav.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../App.css';
 import Logo from '../cosmologo.png';
 import { NavLink } from 'react-router-dom';
+import { Menu } from 'semantic-ui-react';
 
-function Nav() {
+/*function Nav() {
   return (
     <div><nav>
           <NavLink className="link-style" to="/">
@@ -30,4 +31,69 @@ function Nav() {
   );
 }
 
-export default Nav;
+export default Nav;*/
+
+
+import { Input } from 'semantic-ui-react'
+import { Sticky } from 'semantic-ui-react'
+export default class MenuExampleSecondary extends Component {
+  state = { activeItem: 'home' }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <div class="sticky">
+        
+      <Menu secondary>
+         <NavLink className="menuLogo" to="/">
+        <Menu.Item
+            name='logo'
+            active={activeItem === 'logo'}
+            onClick={this.handleItemClick}
+          />
+          </NavLink>
+        <Menu.Menu position='right'>
+         <NavLink className="menuLinks" to="/">
+        <Menu.Item
+          name='home'
+          active={activeItem === 'home'}
+          onClick={this.handleItemClick}
+        />
+         </NavLink>
+         <NavLink className="menuLinks" to="/fossilfuel">
+        <Menu.Item
+          name='fossil fuel'
+          active={activeItem === 'fossilfuel'}
+          onClick={this.handleItemClick}
+        />
+         </NavLink>
+         <NavLink className="menuLinks" to="/temperature">
+        <Menu.Item
+          name='temperature'
+          active={activeItem === 'temperature'}
+          onClick={this.handleItemClick}
+        />
+         </NavLink>
+         <NavLink className="menuLinks" to="/glacier">
+        <Menu.Item
+          name='glacier'
+          active={activeItem === 'glacier'}
+          onClick={this.handleItemClick}
+        />
+         </NavLink>
+         <NavLink className="menuLinks" to="/sealevel">
+        <Menu.Item
+          name='sea level'
+          active={activeItem === 'sealevel'}
+          onClick={this.handleItemClick}
+        />
+        </NavLink>
+        </Menu.Menu>
+      </Menu>
+      </div>
+    )
+  }
+}
