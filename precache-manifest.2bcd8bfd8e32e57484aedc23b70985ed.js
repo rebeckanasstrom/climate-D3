501a0dda96a38256b906df6ed2c5eea57954566d
malinaastrom <56342796+malinaastrom@users.noreply.github.com>
2020-01-08 11:27:38 +0100

    Updates

---
+++ precache-manifest.2bcd8bfd8e32e57484aedc23b70985ed.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f64c74a4a156f7a51f7b5f562cef9362",
+    "url": "/climate-D3/index.html"
+  },
+  {
+    "revision": "a402234d709967e166c1",
+    "url": "/climate-D3/static/css/2.a8952bea.chunk.css"
+  },
+  {
+    "revision": "fa5f539ac16e6b099928",
+    "url": "/climate-D3/static/css/main.04a2d1ae.chunk.css"
+  },
+  {
+    "revision": "a402234d709967e166c1",
+    "url": "/climate-D3/static/js/2.8ea76aa7.chunk.js"
+  },
+  {
+    "revision": "fa5f539ac16e6b099928",
+    "url": "/climate-D3/static/js/main.b1c281b9.chunk.js"
+  },
+  {
+    "revision": "9451a4891f4683f70d8c",
+    "url": "/climate-D3/static/js/runtime-main.c09df392.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/climate-D3/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "65363c4d55617bbeb57d8ce6dcd46099",
+    "url": "/climate-D3/static/media/brand-icons.65363c4d.svg"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/climate-D3/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/climate-D3/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/climate-D3/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "d591c81c6072e55aa274ad24f1c766c9",
+    "url": "/climate-D3/static/media/cleaning.d591c81c.png"
+  },
+  {
+    "revision": "a0a3f6f137a0c266d77df36a09e3136f",
+    "url": "/climate-D3/static/media/cosmetics.a0a3f6f1.png"
+  },
+  {
+    "revision": "ae3b5e8baeabd644ff1326f8a2ad8354",
+    "url": "/climate-D3/static/media/cosmologo.ae3b5e8b.png"
+  },
+  {
+    "revision": "7c595f41c548ba643c35e10d7e7b3fa7",
+    "url": "/climate-D3/static/media/data-step.7c595f41.png"
+  },
+  {
+    "revision": "55e0b11eb32435023714a765e0c4c293",
+    "url": "/climate-D3/static/media/diy.55e0b11e.png"
+  },
+  {
+    "revision": "21e000f43c18df4fbfb5a92f863a618a",
+    "url": "/climate-D3/static/media/dress.21e000f4.png"
+  },
+  {
+    "revision": "a9b0f5d6a6e07a0359f0102759cafd93",
+    "url": "/climate-D3/static/media/drylandscape.a9b0f5d6.jpg"
+  },
+  {
+    "revision": "0925d6f1a6bf659085c4d9c2dc698c40",
+    "url": "/climate-D3/static/media/factory.0925d6f1.jpg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/climate-D3/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "3d9cb43dee891121531a76ce4e55b2b8",
+    "url": "/climate-D3/static/media/flood.3d9cb43d.jpg"
+  },
+  {
+    "revision": "f25e9d0d81ee8d496b69ad245b892ac4",
+    "url": "/climate-D3/static/media/fossilfuel.f25e9d0d.jpg"
+  },
+  {
+    "revision": "f6192aa5fcdd5dda6714385cb19e6f8d",
+    "url": "/climate-D3/static/media/friends.f6192aa5.png"
+  },
+  {
+    "revision": "8601eaefafda0680ccc6565cab674367",
+    "url": "/climate-D3/static/media/glaciers.8601eaef.jpg"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/climate-D3/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "448f2aaa315fa9dce7b2cf6ce31caed6",
+    "url": "/climate-D3/static/media/icons.448f2aaa.svg"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/climate-D3/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/climate-D3/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/climate-D3/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "ff2cbebccda8a89090ba7caadd668b6f",
+    "url": "/climate-D3/static/media/inspire.ff2cbebc.png"
+  },
+  {
+    "revision": "c695321a67dcf16aa8a675162eeb548b",
+    "url": "/climate-D3/static/media/meltingice.c695321a.jpg"
+  },
+  {
+    "revision": "ce3d657c61e1d08653c7f0715497709c",
+    "url": "/climate-D3/static/media/mountain-hero.ce3d657c.jpg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/climate-D3/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/climate-D3/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "b551b554a67e86a840bc80cbb8066c30",
+    "url": "/climate-D3/static/media/outline-icons.b551b554.svg"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/climate-D3/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/climate-D3/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "18f68de614dad04791072a249dd1a36e",
+    "url": "/climate-D3/static/media/periodcup.18f68de6.png"
+  },
+  {
+    "revision": "e0bda2212689d582203ff9d1e4ce0384",
+    "url": "/climate-D3/static/media/plastic.e0bda221.png"
+  },
+  {
+    "revision": "99a52f1eb27a7537a504f8abc888d598",
+    "url": "/climate-D3/static/media/read.99a52f1e.png"
+  },
+  {
+    "revision": "f31ee868bc7c61a4c2512803dc9e7167",
+    "url": "/climate-D3/static/media/save.f31ee868.png"
+  },
+  {
+    "revision": "bf6f3beed7552a950470e9730638c830",
+    "url": "/climate-D3/static/media/sealevels.bf6f3bee.jpg"
+  },
+  {
+    "revision": "d043aacb646ce6f3075c2933d24a9a62",
+    "url": "/climate-D3/static/media/soy-milk.d043aacb.png"
+  },
+  {
+    "revision": "cad076d705fd15c315fba9f01a7883b6",
+    "url": "/climate-D3/static/media/sprout.cad076d7.png"
+  },
+  {
+    "revision": "cdadc61722d960a7730e7ca4c814d5c7",
+    "url": "/climate-D3/static/media/subway.cdadc617.png"
+  },
+  {
+    "revision": "16f8b4400d1e616d102fb2a77950cd10",
+    "url": "/climate-D3/static/media/temp.16f8b440.jpg"
+  },
+  {
+    "revision": "15f6d0b07d2ddf94ec54b3c152bcf816",
+    "url": "/climate-D3/static/media/tools.15f6d0b0.png"
+  },
+  {
+    "revision": "33638cd1a7a2b4affbaa812c5af9dec4",
+    "url": "/climate-D3/static/media/vote.33638cd1.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/climate-D3/precache-manifest.81c8d107d5a62c25a8eca5f3a0dca67f.js"
+  "/climate-D3/precache-manifest.2bcd8bfd8e32e57484aedc23b70985ed.js"
 );
 
 self.addEventListener('message', (event) => {
